411842115d025c352c2a659435d10dbcbfc1b554
MedAb94 <medab.vall@gmail.com>
2020-12-10 11:13:24 +0000

    init commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+
+    render: h => h(App),
+    created() {
+        AOS.init({
+            duration: 800,
+            easing: "ease-in-out",
+            once: true
+        });
+    },
 }).$mount('#app')
