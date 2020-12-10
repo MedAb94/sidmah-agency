13fe1197e8f5f993650c19634f73c293e4cd6cac
MedAb94 <medab.vall@gmail.com>
2020-12-10 17:25:34 +0000

    design

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 import 'bootstrap'
-import AOS from "aos";
 import "aos/dist/aos.css";
+import AOS from "aos";
+
 
 Vue.config.productionTip = false
 
 new Vue({
-
     render: h => h(App),
     created() {
         AOS.init({
